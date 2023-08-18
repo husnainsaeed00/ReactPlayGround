99b14fc13a94e9bae22c2fcbee04d5c8a7fb8b3f
Husnain Saeed <husnainsaeed000@gmail.com>
2023-08-18 04:58:15 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+export default function App(){
+  //Use State Hook returns an arry we are using Array destructuring one variable is used to store the value and other one is used for the 
+  //definition of the funtions to be used in the state
+  const [count, setCount]=React.useState(0)
+  function Add(){
+    setCount(count+1)
+  }
+  function minus(){
+    setCount(count-1)
+  }
+  return(
+    <div>
+      <button onClick={Add}>AddNumber</button>
+      <button onClick={minus}>MinusNumber</button>
+      <h1>{count}</h1>
+   </div>
+  )
+}
